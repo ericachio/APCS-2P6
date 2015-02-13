6845608e5036d24ed5865e69bb59c6ada5390027
Erica Chio <ericanewyork@gmail.com>
2015-02-12 20:24:04 -0500

    classwork folder created

---
+++ Work/Driver.java
@@ -0,0 +1,27 @@
+public class Driver{
+
+    //for mazesolver
+    
+    public static void main(String[]args){
+	Mazesolver f;
+	if(args.length < 1){
+	    f = new Mazesolver("data1.dat");
+	}else{
+	    f = new Mazesolver(args[0]);
+	}
+	f.clearTerminal();
+	f.solve();
+				
+    }
+    
+    /*
+    public static void main(String[] args){
+	KnightsTour a;
+	a = new KnightsTour(4);
+	a.clearTerminal();
+	a.solve();
+	System.out.println(a.toString());
+    }
+    */
+
+}
