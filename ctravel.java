bc19a0b1e5125c2d9b13eef88abf514122e1f584
Erica Chio <ericanewyork@gmail.com>
2015-02-24 12:34:21 -0500

    woo

---
+++ ctravel.java
@@ -0,0 +1,9 @@
+public class ctravel{
+    public static int solve(int[] one, int[][] two, int[] three){
+	
+    }
+
+    public static boolean helps(int[] one, int[][] two, int[] three){
+	
+    }
+}
