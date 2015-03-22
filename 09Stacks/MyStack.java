b376bde6cc0f615aa6ca71aa8c8c94bf227f9fdb
Erica Chio <ericanewyork@gmail.com>
2015-03-22 13:24:56 -0400

    woo starting stacks

---
+++ 09Stacks/MyStack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class MyStack<T>{
+
+    private LinkedList<T> list;
+
+    public T push(T item){
+
+    }
+
+    public T pop(){
+	
+    }
+
+    public T peek(){
+
+    }
+
+    public boolean empty(){
+
+    }
+}
