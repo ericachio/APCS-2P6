e8f0327a3bb2017d8789957cf0d13663e4d41bb8
Erica Chio <ericanewyork@gmail.com>
2015-03-29 11:48:47 -0400

    2 warnings, 2 more to achieve

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class MyDeque<T>{
 
+    public Object[] list;
+    public int head;
+    public int tail;
+    public int size;
+    
+    public MyDeque(){
+	list = new Object[10];
+	head = 1;
+	tail = 0;
+    }
+    
     public void addFirst(T value){
-
+	
     }
     public void addLast(T value){
 
@@ -13,10 +26,10 @@ public class MyDeque<T>{
 
     }
     public T getFirst(){
-
+	return (T) list[head];
     }
     public T getLast(){
-
+	return (T) list[tail];
     }
     
 }
